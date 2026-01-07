b62895f60b06ca82ea6b2ebd050fa6ad1aa87229
Dennis van der Stelt <dvdstelt@gmail.com>
2026-01-07 12:52:10 +0100

    New layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://usewhatworks.dev',
+  integrations: [tailwind()],
   build: {
     assets: 'assets'
   }
